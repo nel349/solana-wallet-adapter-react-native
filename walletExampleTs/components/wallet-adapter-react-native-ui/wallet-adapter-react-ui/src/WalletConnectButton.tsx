d62f16ead7144e195d3de02dcd8c89d29b596b62
Norman Lopez <noell.lpz@gmail.com>
2022-06-04 11:46:41 -0500

    Fixed metro config issues. Added cjs file, added     "@solana/wallet-adapter-react": "^0.15.3"

--- walletExampleTs/components/wallet-adapter-react-native-ui/wallet-adapter-react-ui/src/WalletConnectButton.tsx
+++ walletExampleTs/components/wallet-adapter-react-native-ui/wallet-adapter-react-ui/src/WalletConnectButton.tsx
@@ -1,13 +1,46 @@
-import { useWallet } from '@solana/wallet-adapter-react';
-import React, {FC} from 'react';
+import {useWallet} from '@solana/wallet-adapter-react';
+import React, {FC, useCallback, useMemo} from 'react';
 import {ButtonProps} from 'react-native';
 import Button from './Button';
 
-export const WalletConnectButton: FC<ButtonProps> = ({onPress, ...props}) => {
+export const WalletConnectButton: FC<ButtonProps> = ({
+  onPress,
+  children,
+  ...props
+}) => {
+  const {wallet, connect, connecting, connected} = useWallet();
+
+  const handlePress = useCallback(
+    event => {
+      if (onPress) {
+        onPress(event);
+      }
+      if (!event.defaultPrevented) {
+        connect().catch(() => {});
+      }
+    },
+    [onPress, connect],
+  );
+
+  const content = useMemo(() => {
+    if (children) {
+      return children;
+    }
+    if (connecting) {
+      return 'Connecting ...';
+    }
+    if (connected) {
+      return 'Connected';
+    }
+    if (wallet) {
+      return 'Connect';
+    }
+    return 'Connect Wallet';
+  }, [children, connecting, connected, wallet]);
 
   return (
-    <Button disabled={false} onPress={onPress} {...props}>
-      Connect {/*  //{content} */}
+    <Button disabled={false} onPress={handlePress} {...props}>
+      {content}
     </Button>
   );
 };
